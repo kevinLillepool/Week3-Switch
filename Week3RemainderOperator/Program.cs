f17e88a7c0eec9d28fb0fb8e670a351d6aa4bfd6
Kevin Lillepool <kevin.lillepool@tktk.ee>
2024-10-27 17:25:36 +0200

    Week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿//Odd or even
+
+int a = 110;
+
+int result = a % 2;
+
+Console.WriteLine(result);  
+
+//if (result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//} else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
+
+switch (result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1: Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+
+}
